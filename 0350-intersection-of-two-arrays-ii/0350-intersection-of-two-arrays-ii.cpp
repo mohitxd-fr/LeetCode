159785d5d5d53fa03a6559dc8c41af42db166a83
Mohit Budhlakoti <kartikbudhlakoti3@gmail.com>
2024-07-02 09:29:36 +0530

    Time: 3 ms (78.74%), Space: 12.6 MB (75.54%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -6,17 +6,27 @@ public:
         int m = nums2.size();
         vector<int> v;
 
-        for(int i=0;i<n;i++)
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+
+        int i = 0;
+        int j = 0;
+
+        while(i < n && j < m)
         {
-            mp[nums1[i]]++;
-        }
-        for(int i=0;i<m;i++)
-        {
-            auto it = mp.find(nums2[i]);
-            if(it != mp.end() && it-> second != 0)
+            if(nums1[i] == nums2[j])
             {
-                v.push_back(it-> first);
-                it-> second--;
+                v.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+            else if(nums1[i] < nums2[j])
+            {
+                i++;
+            }
+            else
+            {
+                j++;
             }
         }
         return v;
