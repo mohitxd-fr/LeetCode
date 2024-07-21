3825d2dfd42394d1ddd002fd5142b9218d4b1a38
Mohit Budhlakoti <kartikbudhlakoti3@gmail.com>
2024-07-21 08:41:05 +0530

    Time: 44 ms (100%), Space: 17.5 MB (20%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -4,8 +4,7 @@ public:
         int count = 0;
         for(char ch : s)
         {
-            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || 
-               ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
             {
                 count++;
             }
