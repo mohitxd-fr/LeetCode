f65725b9a8b1653684cf7cb2798eed004d9417f8
Mohit Budhlakoti <kartikbudhlakoti3@gmail.com>
2024-07-29 18:37:53 +0530

    Time: 13 ms (18.9%), Space: 16.2 MB (7.09%) - LeetHub

--- 1486-find-the-distance-value-between-two-arrays/1486-find-the-distance-value-between-two-arrays.cpp
+++ 1486-find-the-distance-value-between-two-arrays/1486-find-the-distance-value-between-two-arrays.cpp
@@ -3,8 +3,6 @@ public:
     int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
         sort(arr1.begin(), arr1.end());
         sort(arr2.begin(), arr2.end());
-        // int i = 0;
-        // int j = 0;
         int count = 0;
 
         for (int i = 0; i < arr1.size(); i++)
